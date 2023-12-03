eee322eadf6c06e23756a19e825425fcd84fcc04
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-12-03 08:33:30 +0100

    icons  back

--- app/components/LinkBar.js
+++ app/components/LinkBar.js
@@ -1,35 +1,22 @@
 import { VercelIcon, GithubIcon } from "../icons";
 
 const LinkBar = () => (
-  <div>
-    <div>
+  <div className="absolute top-5 hidden w-full justify-between px-5 sm:flex">
     <a
-        href="/adex"
-        target="_blank"
-        className="rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto"
-      >
-        <img src="..\AdEx_Beratung_LOGO_color_RGB.svg" alt="AdEx Logo" />
-      </a>  
-
-      <div>
-        AdEx <b>SalesGuru</b> - via secure OpenAI Assistant API
-      </div>
-
-
-      
-      <a
-        href="/github"
-        target="_blank"
-        className="rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto"
-      >
-        <GithubIcon />
-      </a>
-    </div>
-    <div>
-      Placeholder for Default
-    </div>
+      href="/deploy"
+      target="_blank"
+      className="rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto"
+    >
+      <VercelIcon />
+    </a>
+    <a
+      href="/github"
+      target="_blank"
+      className="rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto"
+    >
+      <GithubIcon />
+    </a>
   </div>
-
 );
 
 export default LinkBar;
