bb942843dcd38652246c927f9905bd7a1fcaf826
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-12-03 08:22:44 +0100

    app\AdEx_Beratung_LOGO_color_RGB.svg

--- app/components/LinkBar.js
+++ app/components/LinkBar.js
@@ -8,12 +8,15 @@ const LinkBar = () => (
         target="_blank"
         className="rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto"
       >
-        <AdExIcon />
-      </a>      
+        <img src="..\AdEx_Beratung_LOGO_color_RGB.svg" alt="AdEx Logo" />
+      </a>  
 
       <div>
         AdEx <b>SalesGuru</b> - via secure OpenAI Assistant API
       </div>
+
+
+      
       <a
         href="/github"
         target="_blank"
