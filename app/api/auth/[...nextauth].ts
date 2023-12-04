6422bb193e9fb8d300ab3874211ad6684edc1b1d
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-12-04 15:59:20 +0100

    AzureADProvider

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -5,9 +5,9 @@ import AzureADProvider from "next-auth/providers/azure-ad";
 export default NextAuth({
   providers: [
     AzureADProvider({
-      clientId: process.env.AZURE_AD_CLIENT_ID,
-      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
-      tenantId: process.env.AZURE_AD_TENANT_ID,
+      clientId: process.env.AZURE_AD_CLIENT_ID ?? "",
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET ?? "",
+      tenantId: process.env.AZURE_AD_TENANT_ID ?? "",
       // You might need to add authorization URL if the default one doesn't work for your case.
       // authorization: { url: "https://login.microsoftonline.com/common/oauth2/v2.0/authorize" },
     }),
