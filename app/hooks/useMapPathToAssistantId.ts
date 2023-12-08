068f68bd8281d3d0d46630fd3d37c7b08371f3e8
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-12-08 07:53:46 +0100

    console.log("path:",path,"assistant config:",config);

--- app/hooks/useMapPathToAssistantId.ts
+++ app/hooks/useMapPathToAssistantId.ts
@@ -5,11 +5,11 @@ export const useMapPathToAssistantId = (path: string) => {
   const [assistantId, setAssistantId] = useState<string | null>(null);
 
   useEffect(() => {
+    console.log("path:",path,"assistant config:",config);
+
     const config = env.NEXT_PUBLIC_ASSISTANTS_CONFIG.find(
       (config) => config.urlPath === path,
     );
-
-    console.log("path:",path,"assistant config:",config);
 
     if (!config) {
       setAssistantId(null);
