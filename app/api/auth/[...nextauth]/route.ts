12197a3ceff5649cfdf7345fe4edf1729022447b
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-12-12 15:01:46 +0100

    auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -27,47 +27,6 @@ declare module "next-auth" {
   // }
 }
 
-/**
-* Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
-*
-  callbacks: {
-    async signIn({ user, account, profile }) {
-      if (!user?.email) {
-        console.error("Email is required for sign in");
-        return false; // Prevent sign-in
-      }
-      console.log("Sign-In: ",user.email);
-      const dateKey = new Date().toISOString().slice(0, 7); // "YYYY-MM"
-      await incrementSignInCount(user.email, dateKey);
-      return true;
-    },
-    async session({ session, token }) {
-      const userId = token.sub ?? 'default-sub-value';
-
-      // Extend session object here
-      session.user = {
-        ...session.user,
-        id: userId,
-      };
-
-      // Assuming the email is stored in the token and not directly in the session.user object
-      if (!token?.email) {
-        console.error("Email is required for session handling");
-        // Modify the session object as needed or return a modified session
-        // For example, you might want to set a flag indicating an incomplete session
-        session.error = "Email is missing";
-        return session; // Return the modified session
-      }
-      console.log("Session-Refresh: ",token.email);
-      const dateKey = new Date().toISOString().slice(0, 7); // "YYYY-MM"
-      await incrementSessionRefreshCount(token.email, dateKey);
-
-      return session;
-    },
-    // Add other callbacks with async as needed
-  },
-* @see https://next-auth.js.org/configuration/options
-*/
 const authOptions: NextAuthOptions = {
   callbacks: {
     session: async ({ session, token }) => {
