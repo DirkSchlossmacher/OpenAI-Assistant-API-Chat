6d101f86fc332cea98064ae7112cc0038c30eb1e
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-12-12 15:36:18 +0100

    assistant

--- app/components/LinkBar.js
+++ app/components/LinkBar.js
@@ -3,7 +3,7 @@ import { VercelIcon, GithubIcon, AdExLogo } from "../icons";
 const LinkBar = () => (
   <div className="absolute top-5 hidden w-full justify-between px-5 sm:flex">
     <a
-      href="/deploy"
+      href="https://www.adexpartners.com"
       target="_blank"
       className="rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto"
     >
--- app/hooks/useChatState.ts
+++ app/hooks/useChatState.ts
@@ -23,7 +23,7 @@ export const useChatState = (defaultAssistentId: string) => {
   const [isStartLoading, setStartLoading] = useState(false);
   const [isSending, setIsSending] = useState(false);
   const [initialThreadMessage, setInitialThreadMessage] = useState(
-    "You are a super-smart and experienced assistant for all at AdEx Partners who aim to identify project opportunities at prospects and existing clients! Introduce yourself and give examples how you can help to the user. Suggest sample questions and explain the required, suited context per question in order to provide good assistance.",
+    "You are a super-smart and experienced assistant for all at AdEx Partners who aim to identify project opportunities at prospects and existing clients! Introduce yourself and give examples how you can help to the user. Suggest sample questions and explain the required, suited context per question in order to provide good assistance. Additionally: - explain how users can use you by copy/paste unstructured input (like: chat conversations, email exchanges, etc), to identify opportunities, to obtain guidance for next steps, ...",
   );
   const [statusMessage, setStatusMessage] = useState("");
   const counter = useRef(0);
