8023be8f6a3d44936849988c7420465aacc28cb8
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-12-12 15:56:09 +0100

    LinkBar

--- app/components/LinkBar.js
+++ app/components/LinkBar.js
@@ -11,25 +11,26 @@ const LinkBar = () => {
 
   // Remove the leading slash if it exists to get the "SalesGuru" part
   const extractedPath = pathName.startsWith('/') ? pathName.substring(1) : pathName;
-}
-return(
-  <div className="absolute top-5 hidden w-full justify-between px-5 sm:flex">
-    <a
-      href="https://www.adexpartners.com"
-      target="_blank"
-      className="rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto"
-    >
-      <AdExLogo />
-      <div>{extractedPath}</div>
-    </a>
-    <a
-      href="/github"
-      target="_blank"
-      className="rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto"
-    >
-      <GithubIcon />
-    </a>
-  </div>
-);
+
+    return(
+    <div className="absolute top-5 hidden w-full justify-between px-5 sm:flex">
+      <a
+        href="https://www.adexpartners.com"
+        target="_blank"
+        className="rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto"
+      >
+        <AdExLogo />
+        <div>{extractedPath}</div>
+      </a>
+      <a
+        href="/github"
+        target="_blank"
+        className="rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto"
+      >
+        <GithubIcon />
+      </a>
+    </div>
+  );
+};
 
 export default LinkBar;
