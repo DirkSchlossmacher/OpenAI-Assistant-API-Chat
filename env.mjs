c5290424f8dcf1ffd75e761d9531fa078197dc8c
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-12-20 11:58:31 +0100

    initialThreadMessage

--- env.mjs
+++ env.mjs
@@ -4,6 +4,7 @@ import { z } from "zod";
 const AssistantsConfigSchema = z.array(z.object({
     "assistantId": z.string(),
     "urlPath": z.string(),
+    "initialThreadMessage": z.string().optional(),
     "restriction": z.enum(["none", "emails"]).optional(),
     "emails": z.array(z.string().email()),
 }))
